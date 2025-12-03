283362cb37e4d7aa8a87b3de37724114e32d35e5
Paul Otieno <vitalispaul48@live.com>
2025-12-03 23:05:45 +0300

    Initializing Structures

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+struct Movie {
+    string title; // No need to initialize strings because they are set to "" by default
+    int releaseYear = 0;
+    bool isPopular; // No need to initialize booleans because they are set to false by default
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Movie movie = {"Terminator", 1984};
+
+    cout << movie.releaseYear;
     return 0;
 }
