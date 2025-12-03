ce44871c17a2b1bf511a255c43ca3fb766bbe2a5
Paul Otieno <vitalispaul48@live.com>
2025-12-04 00:26:10 +0300

    Overloading and streams

--- main.cpp
+++ main.cpp
@@ -9,11 +9,23 @@ struct Movie {
 
 };
 
+struct Point {
+    double x;
+    double y;
+};
+
 bool operator==( const Movie& first, const Movie& second) {
     return (
         first.title == second.title &&
         first.releaseYear == second.releaseYear &&
         first.isPopular == second.isPopular
+    );
+}
+
+bool operator==(const Point& address1, const Point& address2) {
+    return (
+        address1.x == address2.x &&
+        address1.y == address2.y
     );
 }
 
@@ -24,16 +36,32 @@ ostream& operator<<(ostream& stream, const Movie& movie) {
     return stream; // We return the stream so that we can chain the stream insertion operators
 }
 
+ostream& operator<<(ostream& stream, const Point& point) {
+    return stream << '(' << point.x << ", " << point.y << ')';
+}
 
 int main() {
 
     Movie movie1 = {"Terminator", 1984};
 
     Movie movie2 = {"Terminator", 1984};
-    cout << movie1.releaseYear << endl;
+    // cout << movie1.releaseYear << endl;
+
+
+    Point pointOne = {17.12, 29.32};
+    Point pointTwo {38.32, 21.32};
 
     if (movie1 == movie2)
         cout << "Equal" << endl;
-    cout << movie1; // Will give the Title since we returned the movie.title to the output stream
+    cout << movie1 << endl; // Will give the Title since we returned the movie.title to the output stream
+
+    if (pointOne == pointTwo) {
+        cout << "Points are equal" <<endl;
+    } else {
+        cout << "Points aren't equal" <<endl;
+    }
+
+    cout << "Point One: " << pointOne << endl;
+    cout << "Point Two: " << pointTwo << endl;
     return 0;
 }
