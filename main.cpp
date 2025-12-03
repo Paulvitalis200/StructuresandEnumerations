edcefe65dd476be192fda8e95e46667bc1be1297
Paul Otieno <vitalispaul48@live.com>
2025-12-04 00:07:29 +0300

    Output stream

--- main.cpp
+++ main.cpp
@@ -7,25 +7,31 @@ struct Movie {
     int releaseYear = 0;
     bool isPopular; // No need to initialize booleans because they are set to false by default
 
-    // Methods
-    // A method is a function that is part of an object
-    bool equals(const Movie& movie) {
-        return (
-            title == movie.title &&
-            releaseYear == movie.releaseYear &&
-            isPopular == movie.isPopular
-        );
-    }
 };
+
+bool operator==( const Movie& first, const Movie& second) {
+    return (
+        first.title == second.title &&
+        first.releaseYear == second.releaseYear &&
+        first.isPopular == second.isPopular
+    );
+}
+
+// Stream insertion operator
+// ostream is short for output stream
+ostream& operator<<(ostream& stream, const Movie& movie) {
+    stream << movie.title;
+    return stream; // We return the stream so that we can chain the stream insertion operators
+}
+
 
 int main() {
 
     Movie movie1 = {"Terminator", 1984};
 
     Movie movie2 = {"Terminator", 1984};
-    cout << movie1.releaseYear;
+    cout << movie1.releaseYear << endl;
 
-    if (movie1.equals(movie2))
-        cout << "Equal";
+    cout << movie1; // Will give the Title since we returned the movie.title to the output stream
     return 0;
 }
