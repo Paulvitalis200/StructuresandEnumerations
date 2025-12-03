6b06f34349f9b3f136b1804f3c9d5885f375ef6b
Paul Otieno <vitalispaul48@live.com>
2025-12-03 22:56:19 +0300

    Define Structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+// PascalCase
+struct Movie {
+    string title;
+    int releaseYear;
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Movie movie;
+    movie.title = "Terminator";
+    movie.releaseYear = 1984;
+
+    cout << "Title: " << movie.title << endl;
+    cout << "Release year: " << movie.releaseYear << endl;
+
     return 0;
 }
