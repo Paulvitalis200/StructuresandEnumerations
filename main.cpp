ac06996fc568f195c529263370034cb9101b5316
Paul Otieno <vitalispaul48@live.com>
2025-12-03 23:35:48 +0300

    Method

--- main.cpp
+++ main.cpp
@@ -6,12 +6,26 @@ struct Movie {
     string title; // No need to initialize strings because they are set to "" by default
     int releaseYear = 0;
     bool isPopular; // No need to initialize booleans because they are set to false by default
+
+    // Methods
+    // A method is a function that is part of an object
+    bool equals(const Movie& movie) {
+        return (
+            title == movie.title &&
+            releaseYear == movie.releaseYear &&
+            isPopular == movie.isPopular
+        );
+    }
 };
 
 int main() {
 
-    Movie movie = {"Terminator", 1984};
+    Movie movie1 = {"Terminator", 1984};
 
-    cout << movie.releaseYear;
+    Movie movie2 = {"Terminator", 1984};
+    cout << movie1.releaseYear;
+
+    if (movie1.equals(movie2))
+        cout << "Equal";
     return 0;
 }
