6b5606090185a59788deabcb85a6febe6bcde95f
Paul Otieno <vitalispaul48@live.com>
2026-01-29 10:27:19 +0300

    Nested Structures

--- main.cpp
+++ main.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
+struct Date {
+    int year = 1900;
+    int month = 1;
+    short day = 1;
+};
 struct Movie {
     string title; // No need to initialize strings because they are set to "" by default
-    int releaseYear = 0;
+    Date releaseDate;
     bool isPopular; // No need to initialize booleans because they are set to false by default
+
 };
 
 int main() {
 
-    Movie movie = {"Terminator", 1984};
+    Date date {1984, 12, 3};
+    Movie movie = {"Terminator",
+        {
+            1938,
+            2,
+            2
+        }};
 
-    cout << movie.releaseYear;
+    cout << movie.releaseDate.month;
     return 0;
 }
